78ce98ea95e8e37b98b57c58f435328f8e1e091d
Ammar Meshaal <157367715+Ammarrs@users.noreply.github.com>
2026-09-17 10:17:25 +0300

    fix: responsive

--- script.js
+++ script.js
@@ -9,10 +9,12 @@ var calcSub = document.getElementById("calc-sub");
 var calcMult = document.getElementById("calc-mult");
 var calcDec = document.getElementById("calc-dec");
 var calcEqual = document.getElementById("calc-equal");
+var calcErase = document.getElementById("calc-erase");
 var numInput = document.getElementById("num-input");
 var buttons = document.querySelectorAll(".btn");
 var numbersBtns = document.querySelectorAll(".number-btn");
 var operatorsBtns = document.querySelectorAll(".operator-btn");
+
 
 // variables
 
@@ -38,6 +40,9 @@ numbersBtns.forEach(button => {
 });
 
 calcPlus.addEventListener("click", () => { operator = "+"; }); calcSub.addEventListener("click", () => { operator = "-"; }); calcMult.addEventListener("click", () => { operator = "*"; }); calcDiv.addEventListener("click", () => { operator = "/"; });
+calcErase.addEventListener("click", () => {
+    clearInput()
+})
 
 function clearInput() {
     numInput.value = "";
